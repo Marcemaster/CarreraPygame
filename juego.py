44553085ee10f3c185c7ec90805a518f392acb73
Marcelo Hornillos <marcehornillos@gmail.com>
2021-07-13 13:59:46 +0200

    fin de carrera pygame

--- juego.py
+++ juego.py
@@ -4,33 +4,38 @@ import random
 
 class Runner():
     
-    __costumes = ("octopus", "carpincho","turtle", "prawn")
+    __costumes = ("octopus", "fish","turtle", "prawn","moray")
     
-    def __init__(self, x=0, y=0, costume = "turtle"):
-        self.costume = pygame.image.load('images/{}.png'.format(costume))
+    def __init__(self, x=0, y=0):
+        ixCostume = random.randint(0,4)
+        self.costume = pygame.image.load('images/{}.png'.format(self.__costumes[ixCostume]))
         self.position = [x, y]
-        self.name = costume
+        self.name = ""
         
     def avanzar(self):
-        self.position[0] += random.randint(1, 6)
+        self.position[0] += random.randint(1, 2)
 
 
 class Game():
     runners = []
-    __posi = (160, 200, 240, 280)
+    __posi = (140, 180, 220, 260, 300)
     __startLine = 5
     __finishLine = 620
-    __names = ("Octi", "Carpi", "Morrocoy", "Dobladita")
+    __names = ("Octi", "Carpi", "Morrocoy", "Dobladita", "Chingüire")
     
     def __init__(self):
         self.__screen = pygame.display.set_mode((640,480))
-        self.__background = pygame.image.load('images/background.jpg')
+        self.__background = pygame.image.load('images/background.png')
         pygame.display.set_caption("Carrera de bichos")
         
-        for i in range(4):
+        for i in range(5):
             theRunner = Runner(self.__startLine, self.__posi[i])
             theRunner.name = self.__names[i]
             self.runners.append(theRunner)
+            
+    def close():
+        pygame.quit()
+        sys.exit()
         
     def competir(self):
         gameOver = False
@@ -38,26 +43,31 @@ class Game():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     gameOver = True
-            for r in range(4):
-                self.runners[r].avanzar()
-            
-            for r in range(4):
-                if self.runners[r].position[0] >= self.__finishLine:
-                    print("{} ha ganado".format(self.runners[r].name))
-                    gameOver = True
+                    
+            for activerunner in self.runners:
+                activerunner.avanzar()
+                if activerunner.position[0] >= self.__finishLine:
+                    print("{} ha ganado".format(activerunner.name))
+                    gameOver = True                    
             
             self.__screen.blit(self.__background, (0, 0))
-            for r in range(4):
-                self.__screen.blit(self.runners[r].costume, self.runners[r].position)
+            
+            for runner in self.runners:
+                self.__screen.blit(runner.costume, runner.position)
             
             pygame.display.flip()
-
-        pygame.quit()
-        sys.exit()
+            
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.quit():
+                    self.close()
+                
+                
+                
 
 if __name__ == '__main__':
     game = Game()
-    pygame.font.init() 
+    pygame.init() 
     game.competir()
 
 
